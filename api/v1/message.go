c6d3051567aca7ced75aae621b0443cacb04f074
GoldenAmor <wfeng0221@gmail.com>
2022-10-28 15:00:29 +0800

    实现message功能组

--- api/v1/message.go
+++ api/v1/message.go
@@ -3,13 +3,21 @@ package v1
 import (
 	"MIS/models"
 	"MIS/pkg/e"
+	"MIS/pkg/logging"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type ListResponse struct {
 	Response
 	MessageRecords []models.MessageRecord
+}
+
+type MessageListResponse struct {
+	Response
+	MessageList []string
 }
 
 type Response struct {
@@ -31,6 +39,7 @@ func GetMessageList(c *gin.Context) {
 			StatusCode: http.StatusInternalServerError,
 			StatusMsg:  "Database Error!",
 		})
+		return
 	} else {
 		c.JSON(http.StatusOK, ListResponse{
 			Response{
@@ -41,6 +50,77 @@ func GetMessageList(c *gin.Context) {
 		})
 	}
 
+}
+
+func GetMessageByType(c *gin.Context) {
+	messageType := c.Query("message_type")
+	typeParam, err := strconv.Atoi(messageType)
+	if err != nil {
+		logging.Info(err)
+		return
+	}
+	result, err := models.GetMessageByType(typeParam)
+	if err != nil {
+		logging.Info(err)
+		c.JSON(http.StatusInternalServerError, ListResponse{
+			Response:       Response{500, "Failed to get records."},
+			MessageRecords: nil,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, ListResponse{
+		Response:       Response{200, "Get records by message type successfully."},
+		MessageRecords: result,
+	})
+}
+
+func GetMessageDetailByType(c *gin.Context) {
+	messageType := c.Query("message_type")
+	typeParam, err := strconv.Atoi(messageType)
+	if err != nil {
+		logging.Info(err)
+	}
+	result, err := models.GetMessageDetailByType(typeParam)
+	if err != nil {
+		logging.Info(err)
+		c.JSON(http.StatusInternalServerError, ListResponse{
+			Response:       Response{500, "Failed to get records."},
+			MessageRecords: nil,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, MessageListResponse{
+		Response:    Response{200, "Get records by message type successfully."},
+		MessageList: result,
+	})
+}
+
+func DelMessageRecord(c *gin.Context) {
+	userId := c.Query("user_id")
+	targetId := c.Query("target_id")
+	createTime := c.Query("created_at")
+	createAt, err := time.Parse("2006-01-02 15:04:05", createTime)
+	if err != nil {
+		logging.Info(err)
+		c.JSON(http.StatusInternalServerError, Response{
+			StatusCode: 1,
+			StatusMsg:  "failed to convert time string.",
+		})
+		return
+	}
+	err = models.DelMessageRecord(userId, targetId, createAt)
+	if err != nil {
+		logging.Info(err)
+		c.JSON(http.StatusInternalServerError, Response{
+			StatusCode: 2,
+			StatusMsg:  "failed to delete record.",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Response{
+		StatusCode: 200,
+		StatusMsg:  "Successfully delete.",
+	})
 }
 
 //type RecordResponse struct {
--- routers/message.go
+++ routers/message.go
@@ -6,5 +6,8 @@ func MessageRouter() {
 	messages := Apiv1.Group("/message")
 	{
 		messages.GET("/getList", v1.GetMessageList)
+		messages.GET("/getClass", v1.GetMessageByType)
+		messages.GET("/getDetail", v1.GetMessageDetailByType)
+		messages.DELETE("/delete", v1.DelMessageRecord)
 	}
 }
